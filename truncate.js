0c4af841ee914faff9f011558d10c8eb137825d9
Leon Li <la2li@uwaterloo.ca>
2013-11-08 20:04:51 -0500

    Working prototype without method options.

--- truncate.js
+++ truncate.js
@@ -8,80 +8,76 @@ Requires jQuery (for now).
 // function lifted from Stack Overflow user Vitim.us on 2013-11-08 here: http://stackoverflow.com/questions/4009756/
 function occurrences(string, subString, allowOverlapping){
 
-  string+=""; subString+="";
-  if(subString.length<=0) return string.length+1;
+	string+=""; subString+="";
+	if(subString.length<=0) return string.length+1;
 
-  var n=0, pos=0;
-  var step=(allowOverlapping)?(1):(subString.length);
+	var n=0, pos=0;
+	var step=(allowOverlapping)?(1):(subString.length);
 
-  while(true){
-      pos=string.indexOf(subString,pos);
-      if(pos>=0){ n++; pos+=step; } else break;
-  }
-  return(n);
+	while(true){
+		pos=string.indexOf(subString,pos);
+		if(pos>=0){ n++; pos+=step; } else break;
+	}
+	return(n);
 
 }
 
 // jQuery experimental plugin
 (function ( $ ) {
 
-  $.fn.truncate = function() {
+	$.fn.truncate = function() {
 
-    return this.each(function() {
-      var $this = $(this);
-      var originalContent = $this.html();
-      var trimmedContent = originalContent.substr(0, maxLength);
-      var maxLength = 640;
-      var trimLength = maxLength;
-      var morelink = '<button class="btn btn-primary btn-sm btn-block space-10 morelink">more</button>';
-      var lesslink = '<button class="btn btn-primary btn-sm btn-block space-10 lesslink">less</button>';
+	return this.each(function() {
+		var $this = $(this);
+		var originalContent = $this.html();
+		var trimmedContent = originalContent.substr(0, maxLength);
+		var maxLength = 640;
+		var trimLength = maxLength;
+		var ellipsis = ' ...';
+		var morelink = '<button class="btn btn-primary btn-sm btn-block space-10 morelink">more</button>';
+		var lesslink = '<button class="btn btn-primary btn-sm btn-block space-10 lesslink">less</button>';
 
-      if (occurrences(trimmedContent, "<", false) == occurrences(trimmedContent, ">", false)) {
+		if (occurrences(trimmedContent, "<", false) == occurrences(trimmedContent, ">", false)) {
 
-        if (occurrences(trimmedContent, "<a ", false) == occurrences(trimmedContent, "</a>", false)) {
-          trimLength = Math.min( trimmedContent.lastIndexOf(" ",maxLength), trimmedContent.length );
-        }
-        else {
-          trimLength = trimmedContent.lastIndexOf("<a ",maxLength);
-        };
+			if (occurrences(trimmedContent, "<a ", false) == occurrences(trimmedContent, "</a>", false)) {
+				trimLength = Math.min( trimmedContent.lastIndexOf(" ", maxLength), trimmedContent.length );
+			}
+			else {
+				trimLength = trimmedContent.lastIndexOf("<a ", maxLength);
+			};
 
-        trimmedContent = trimmedContent.substr(0, trimLength);
+			trimmedContent = trimmedContent.substr(0, trimLength);
 
-        if (occurrences(trimmedContent, "<p>", false) == (occurrences(trimmedContent, "</p>", false))) {
-          trimmedContent = trimmedContent + morelink;
-        }
-        else {
-          trimmedContent = trimmedContent + ' ...</p>' + morelink;
-        };
+		}
+		else {
+			
+			if (occurrences(trimmedContent, "<a ", false) == occurrences(trimmedContent, "</a>", false)) {
+				trimLength = trimmedContent.lastIndexOf("<", maxLength);
+			}
+			else {
+				trimLength = trimmedContent.lastIndexOf("<a ", maxLength);
+			};
 
-      }
-      else {
-        if (occurrences(trimmedContent, "<a ", false) == occurrences(trimmedContent, "</a>", false)) {
-          trimLength = trimmedContent.lastIndexOf("<",maxLength);
-        }
-        else {
-          trimLength = trimmedContent.lastIndexOf("<a ", maxLength);
-        };
+			trimmedContent = trimmedContent.substr(0, trimLength);
 
-        trimmedContent = trimmedContent.substr(0, trimLength);
+		};
 
-        if (occurrences(trimmedContent, "<p>", false) == (occurrences(trimmedContent, "</p>", false)/2)) {
-          trimmedContent = trimmedContent + morelink;
-        }
-        else {
-          trimmedContent = trimmedContent + ' ...</p>' + morelink;
-        };
-      };
+		if (occurrences(trimmedContent, "<p>", false) == occurrences(trimmedContent, "</p>", false)) {
+			trimmedContent = trimmedContent + morelink;
+		}
+		else {
+			trimmedContent = trimmedContent + ellipsis + '</p>' + morelink;
+		};
 
-      $this.html(trimmedContent);
-      $this.on("click", '.morelink', function(){
-        $this.html(originalContent + '<button class="btn btn-primary btn-sm btn-block space-10 lesslink">less</button>');
-      });
-      $this.on("click", '.lesslink', function(){
-        $this.html(trimmedContent);
-      });
-    });
+		$this.html(trimmedContent);
+		$this.on("click", '.morelink', function(){
+		$this.html(originalContent + lesslink);
+		});
+		$this.on("click", '.lesslink', function(){
+		$this.html(trimmedContent);
+		});
+	});
 
-  };
+	};
 
 }( jQuery ));
